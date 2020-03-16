2020baa6a1c75e0928e6cfdb60330d8620c01533
Paulina Dziobek <dziovek@gmail.com>
2020-03-16 11:24:06 +0100

    home

---
+++ src/components/button/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../pages/Home/style.scss';
+
+export default function Button() {
+  return (
+    <button>Click me</button>
+  );
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../../components/button';
+import './style.scss';
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME</h1>
+      <Button />
+    </>
+  );
+}
